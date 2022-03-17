3a95e380507357c94fd13cd9a3ecc65edf255c2d
Fife <Austin.Fife@fdacs.gov>
2022-03-17 14:07:35 -0400

    adding old datasheets for data extraction

---
+++ ocr_pdfs.R
@@ -0,0 +1,12 @@
+####LOADING PACKAGES AND READING IN FILE LISTS####
+# loading required packages
+pkgs <- c("tesseract")
+
+# installs missing packages
+nu_pkgs <- pkgs[!(pkgs %in% installed.packages()[, "Package"])]
+if (length(nu_pkgs))
+  install.packages(nu_pkgs)
+
+# loading required packages
+lapply(pkgs, library, character.only = TRUE)
+rm(pkgs, nu_pkgs)
