19476d0bf1e9b154b396fa91b1872a604125e9db
Fife <Austin.Fife@fdacs.gov>
2022-03-28 15:08:32 -0400

    developing graph

--- create_graph_tam_yield.R
+++ create_graph_tam_yield.R
@@ -29,8 +29,17 @@ df <-
 df$month <- month(df$date)
 df$year <- year(df$date)
 
-df %>% drop_na() %>% ggplot(aes(x = plant, y = tam_ct, fill = plant)) +
+df %>% drop_na() %>% ggplot(aes(x = plant_common, y = tam_ct, fill = plant_common)) +
   geom_boxplot(notch = TRUE) +
   ggtitle("Total Tamarixia radiata produced, 2020-2022") +
+  geom_text(
+    stat = "count",
+    aes(label = paste0("n = ", ..count..), y = ..count..),
+    position = "fill",
+    size = 7,
+    vjust = 1.5,
+    data = df %>% drop_na()
+  ) +
   theme_tufte(base_size = 30) +
+  theme(axis.title = element_blank(), legend.position = "none") +
   facet_wrap(ggplot2::vars(year))
